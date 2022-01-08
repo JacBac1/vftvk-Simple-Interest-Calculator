612c2da24a834a59db736e92b5df8a09a67f0c2a
JacBac1 <97180497+JacBac1@users.noreply.github.com>
2022-01-08 13:07:01 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function compute()
+function compute()      //button
 {
-    p = document.getElementById("principal").value;
-    
+    //extracted variables
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    //computed variables
+    var interest = principal * years * (rate /100);
+    var amount = parseFloat(principal) + parseFloat(interest);
+    var year = new Date().getFullYear()+parseInt(years);
+    //display, verification
+    if(principal < 0)
+    {
+        document.getElementById("result").innerHTML="The amount you entered is invalid.\<br\>"
+    }
+    else
+    {
+        document.getElementById("result").innerHTML="If you deposit $"+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of $"+amount.toFixed(2)+",\<br\>in the year "+year+"\<br\>"
+    }
+}
+function updateRate()       //slider display
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
         
